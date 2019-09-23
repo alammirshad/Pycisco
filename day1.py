471bbbdc694065cf9561f6e3a06bb88b4f9e1d0a
alammirshad <alam.mirshad@gmail.com>
2019-09-23 13:21:40 +0530

    pre lunch day1 python code.

---
+++ day1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import time
+print("hello world")
+x = 4
+y = "ok google"
+z = [4, 77,x,y] 
+print(y)
+time.sleep(3)
+print(z)
+time.sleep(2)
+print("Current date and time is ",time.ctime())
+
+
